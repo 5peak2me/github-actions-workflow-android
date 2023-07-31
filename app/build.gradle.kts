8e7c2b49c65137dfb4e8032c8a34b8bfa215f55c
dai jinlin <daijinlin@zepp.com>
2023-07-31 15:35:17 +0800

    test cache

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,12 +21,12 @@ plugins {
 
 android {
     namespace = "com.l3gacy.app.sample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.l3gacy.app.sample"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
