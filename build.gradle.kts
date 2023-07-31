f8b9b170a1bb5a653320413a3e726a09a90a774e
dai jinlin <daijinlin@zepp.com>
2023-07-31 13:44:28 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.spotless)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "github-actions-workflow-android"
+include(":app")
