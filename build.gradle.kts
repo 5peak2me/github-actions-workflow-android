65f4a854a17b0c8d695799ea2a15f9034690caf7
dai jinlin <daijinlin@zepp.com>
2023-07-31 15:53:00 +0800

    验证detekt报告上传

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,5 +9,17 @@ plugins {
 
 subprojects {
     apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        ignoreFailures = true
+        reports {
+            html.required.set(true) // observe findings in your browser with structure and code snippets
+            xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
+            txt.required.set(false) // similar to the console output, contains issue signature to manually edit baseline files
+            sarif.required.set(false) // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with GitHub Code Scanning
+            md.required.set(true) // simple Markdown format
+        }
+    }
+
     apply(plugin = "jacoco")
 }
