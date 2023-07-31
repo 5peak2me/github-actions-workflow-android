a344a95009d28e6d2ef5d25e2ce522e9a86d5249
dai jinlin <daijinlin@zepp.com>
2023-07-31 13:55:26 +0800

    detekt 问题

--- app/src/test/java/com/l3gacy/app/sample/ExampleUnitTest.kt
+++ app/src/test/java/com/l3gacy/app/sample/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.l3gacy.app.sample
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
